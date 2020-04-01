4d2256cf7f39f33a194a4a7b69bbd48175f4751e
Udi Talias <udi.talias@gmail.com>
2020-04-01 10:07:26 +0300

    feat(client): enable return key to load URL. Add clear text button to address bar. hide keyboard on load. show loading indicator when loading url.

--- ios/VidazooWebviewTester/ViewController.swift
+++ ios/VidazooWebviewTester/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegate {
 
     @IBOutlet weak var webview: WKWebView!
     
@@ -17,17 +17,58 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var loadBtn: UIButton!
     
+    var indicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        createActivityIndicator()
+        
+        self.webview.navigationDelegate = self
+        
+        self.address.delegate = self
+        self.address.clearButtonMode = .whileEditing
         self.address.text = "https://saas-player-demo-pages.herokuapp.com/in-article.html";
     }
     
-    @IBAction func onLoadClick(_ sender: Any) {
+    func createActivityIndicator() {
+        indicator = UIActivityIndicatorView(style: .gray)
         
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.stopAnimating()
+        view.addSubview(indicator)
+        
+        indicator.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        indicator.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        loadWebview()
+        return false
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        indicator.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        indicator.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        indicator.stopAnimating()
+    }
+    
+    @IBAction func onLoadClick(_ sender: Any) {
+        loadWebview()
+    }
+    
+    private func loadWebview() {
         guard let url = self.address.text else {
             return
         }
+        
+        address.resignFirstResponder()
         
         let urlObj = NSURL(string: url)
         let req = URLRequest(url: urlObj! as URL)
